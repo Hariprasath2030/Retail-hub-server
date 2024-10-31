84e4a2de172d5b0f71a2a96896b67e1f3d3e8366
Hariprasath V <webhari2005@gmail.com>
2024-10-31 16:47:34 +0530

    edit

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 // Connect to MongoDB
 (async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("Connected to MongoDB");
     } catch (error) {
         console.error("Could not connect to MongoDB:", error);
